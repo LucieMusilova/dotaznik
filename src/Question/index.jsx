0cbe213bd601e12a223d9b3e7117ba72e283da25
Lucie Musilová <musil.lucie@seznam.cz>
2021-11-12 16:29:50 +0100

    done

--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
 
-const Question = (props) => (
+
+const Question = ({text}) => {
+
+  const [answered, setAnswered] = useState('symbolQuestion');
+
+
+  const showText = (iconType) => {
+    setAnswered(iconType);
+  }
+
+  return(
   <div className="question">
     <QuestionBody 
-      iconType="symbolQuestion" 
-      text={props.text}
+      iconType={answered ? answered : 'symbolQuestion'} 
+      text={text}
     />
     <div className="question__options">
-      <Option type="smileyStrongYes" text="Souhlasím"/>
-      <Option type="smileyYes" text="Spíše souhlasím"/>
-      <Option type="smileyNeutral" text="Nevím"/>
-      <Option type="smileyNo" text="Spíše nesouhlasím"/>
-      <Option type="smileyStrongNo" text="Nesouhlasím"/>
+      <Option type="smileyStrongYes" text="Souhlasím" show={showText}/>
+      <Option type="smileyYes" text="Spíše souhlasím" show={showText}/>
+      <Option type="smileyNeutral" text="Nevím" show={showText}/>
+      <Option type="smileyNo" text="Spíše nesouhlasím" show={showText}/>
+      <Option type="smileyStrongNo" text="Nesouhlasím" show={showText}/>
     </div>
-  </div>
-);
+  </div>)
+};
 
 export default Question;
